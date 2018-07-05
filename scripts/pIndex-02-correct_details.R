83b22dbe9c21bc21f1f0d4670856ef6c71ede887
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2018-07-05 17:27:01 +1000

    add pIndex-02-correct_details.R script for product corrections

---
+++ scripts/pIndex-02-correct_details.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(here)
+
+library(openxlsx)
+library(readxl)
+
+path2cache <- here("spreadsheets/cache/pIndex/")
+
+index2 <- read_csv(paste0(path2cache, "pIndex-02-1987to1992.csv"))
+
+index2 %>% arrange(review_year, company, product) %>%
+  # select(rowid, review, company, product_brand, product, price, price_year, review_year, pIndex_year) %>%
+  # write_csv(paste0(path2cache, "pIndex-02-handcorrections.csv"))
+
+index2 %>% mutate(
+  release_year = review_year,
+  release_price = price) %>% 
+  select(rowid, review, company, product_brand, product, release_year, release_price, price_year, price) %>%
+  arrange(company, product, price_year) %>% View()
+  write.xlsx(file = paste0(path2cache, "pIndex-02-product_corrections.xlsx"))
